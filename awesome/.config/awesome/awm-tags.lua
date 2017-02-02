2e5c0f8e63c1a133824ead5ecb2c7bf0261f55a7
Pablo Andres Dealbera <dealberapablo07@gmail.com>
2017-02-02 06:49:38 -0300

    Awesome: minor change

--- awesome/.config/awesome/awm-tags.lua
+++ awesome/.config/awesome/awm-tags.lua
@@ -2,7 +2,7 @@ local awful = require("awful")
 -- {{{ Tags
 tags = {
    names = { "", "", "", "", ""},
-   layout = { layouts[2], layouts[5], layouts[5], layouts[5], layouts[6] }
+   layout = { layouts[2], layouts[5], layouts[5], layouts[3], layouts[6] }
 }
 for s = 1, screen.count() do
 	tags[s] = awful.tag(tags.names, s, tags.layout)
