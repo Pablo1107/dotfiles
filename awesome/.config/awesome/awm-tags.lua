f27e0b42339a81da0741f381ca49f685df031b76
Pablo Andres Dealbera <dealberapablo07@gmail.com>
2018-10-27 18:07:51 -0300

    Awesome: Add Steam tag

--- awesome/.config/awesome/awm-tags.lua
+++ awesome/.config/awesome/awm-tags.lua
@@ -1,8 +1,10 @@
+-- For the the tag's icon you have to install ttf-font-awesome
+
 local awful = require("awful")
--- {{{ Tags
+-- {{{ Tags 
 tags = {
-   names = { "", "", "", "", ""},
-   layout = { layouts[2], layouts[5], layouts[5], layouts[5], layouts[6] }
+   names = { "", "", "", "", "", ""},
+   layout = { layouts[2], layouts[5], layouts[5], layouts[5], layouts[6], layouts[5] }
 }
 for s = 1, screen.count() do
 	tags[s] = awful.tag(tags.names, s, tags.layout)
