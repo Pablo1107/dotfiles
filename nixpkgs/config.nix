73ee9b44777c58bae464a8436da4603839616b46
Pablo Andres Dealbera <dealberapablo07@gmail.com>
2021-06-27 23:55:34 -0300

    update nur

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -4,9 +4,9 @@
     nur = import (
       builtins.fetchTarball {
         # Get the revision by choosing a version from https://github.com/nix-community/NUR/commits/master
-        url = "https://github.com/nix-community/NUR/archive/73fc4ff8f984dcf229d8390fb09bc61494c134db.tar.gz";
+        url = "https://github.com/nix-community/NUR/archive/5d143233e53065486c9a768c93bafbc61a1261e1.tar.gz";
         # Get the hash by running `nix-prefetch-url --unpack <url>` on the above url
-        sha256 = "1rkpjrm8mv8s8hz828l04smdp3q3pjy2804fymksyqnay5biddrd";
+        sha256 = "055vp8025w71qbcbj9djr6x8znl73d35pkf2v52j38zd2z6mb32w";
       }
     ) {
       inherit pkgs;
