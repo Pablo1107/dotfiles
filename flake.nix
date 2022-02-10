b8084db1e6a7cb1714b7c2d95f10d0b13505d662
Pablo Andres Dealbera <dealberapablo07@gmail.com>
2022-02-10 02:30:16 -0300

    update flakes

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nur.url = "github:nix-community/NUR";
   };
 
   outputs = inputs: {
@@ -13,7 +14,14 @@
         system = "x86_64-linux";
         homeDirectory = "/home/pablo";
         username = "pablo";
-        configuration.imports = [ ./config/nixpkgs/home.nix ];
+        stateVersion = "21.05";
+        configuration = {
+          imports = [ ./config/nixpkgs/home.nix ];
+          nixpkgs = {
+            config = { allowUnfree = true; };
+            overlays = [ inputs.nur.overlay ];
+          };
+        };
       };
     };
   };
