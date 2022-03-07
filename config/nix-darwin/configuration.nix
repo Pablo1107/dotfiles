72bb02bdafcdf45a9d3e6f10c6785a4c388e8a58
Pablo Andres Dealbera <dealberapablo07@gmail.com>
2022-03-07 11:32:54 -0300

    add nix-darwin config

---
+++ config/nix-darwin/configuration.nix
@@ -0,0 +1,40 @@
+{ config, pkgs, ... }:
+
+{
+  fonts.enableFontDir = true;
+  fonts.fonts = [
+    hack-font
+  ];
+
+  users.users.pablo = {
+    name = "pablo";
+    home = "/Users/pablo";
+  };
+
+  home-manager.users.pablo = { pkgs, ... }: {
+    imports = [ ./config/nixpkgs/home.nix ];
+  };
+
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages =
+    [
+      pkgs.neovim
+    ];
+
+  # Use a custom configuration.nix location.
+  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
+  # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
+
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+  # nix.package = pkgs.nix;
+
+  # Create /etc/bashrc that loads the nix-darwin environment.
+  #programs.zsh.enable = true; # default shell on catalina
+  # programs.fish.enable = true;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+}
--- flake.nix
+++ flake.nix
@@ -5,15 +5,17 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    darwin.url = "github:lnl7/nix-darwin/master";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
     nur.url = "github:nix-community/NUR";
     nixgl.url = "github:guibou/nixGL";
     emacs-overlay.url = "github:nix-community/emacs-overlay";
     declarative-cachix.url = "github:jonascarpay/declarative-cachix";
   };
 
-  outputs = inputs: {
+  outputs = { self, nixpkgs, home-manager, darwin, nur, nixgl, emacs-overlay, declarative-cachix }: {
     homeConfigurations = {
-      pablo = inputs.home-manager.lib.homeManagerConfiguration {
+      pablo = home-manager.lib.homeManagerConfiguration {
         system = "x86_64-linux";
         homeDirectory = "/home/pablo";
         username = "pablo";
@@ -21,18 +23,26 @@
         configuration = {
           imports = [
             ./config/nixpkgs/home.nix
-            inputs.declarative-cachix.homeManagerModules.declarative-cachix
+            declarative-cachix.homeManagerModules.declarative-cachix
           ];
           nixpkgs = {
             config = { allowUnfree = true; };
             overlays = [
-              inputs.nur.overlay
-              inputs.nixgl.overlay
-              inputs.emacs-overlay.overlay
+              nur.overlay
+              nixgl.overlay
+              emacs-overlay.overlay
             ];
           };
         };
       };
+    };
+    darwinConfigurations.Pablos-MacBook-Air = darwin.lib.darwinSystem {
+      system = "aarch64-darwin";
+      modules = [
+        ./config/nix-darwin/configuration.nix
+        home-manager.darwinModules.home-manager
+      ];
+      inputs = { inherit darwin nixpkgs; };
     };
   };
 }
