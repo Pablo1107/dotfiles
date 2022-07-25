76e89622c50dba6631f28635556bc12c5c3b4477
Pablo Andres Dealbera <dealberapablo07@gmail.com>
2022-07-24 23:04:38 -0300

    nix: update darwin config

--- config/nix-darwin/configuration.nix
+++ config/nix-darwin/configuration.nix
@@ -117,23 +117,24 @@ with pkgs;
       cmd - 7 : yabai -m space --focus 7
       cmd - 8 : yabai -m space --focus 8
 
-      shift + cmd - 1 : yabai -m window --space 1
-      shift + cmd - 2 : yabai -m window --space 2
-      shift + cmd - 3 : yabai -m window --space 3
-      shift + cmd - 4 : yabai -m window --space 4
-      shift + cmd - 5 : yabai -m window --space 5
-      shift + cmd - 6 : yabai -m window --space 6
-      shift + cmd - 7 : yabai -m window --space 7
-      shift + cmd - 8 : yabai -m window --space 8
+      # shift + cmd - 1 : yabai -m window --space 1
+      # shift + cmd - 2 : yabai -m window --space 2
+      # shift + cmd - 3 : yabai -m window --space 3
+      # shift + cmd - 4 : yabai -m window --space 4
+      # shift + cmd - 5 : yabai -m window --space 5
+      # shift + cmd - 6 : yabai -m window --space 6
+      # shift + cmd - 7 : yabai -m window --space 7
+      # shift + cmd - 8 : yabai -m window --space 8
 
       # control window size, modified to be intuitive
       cmd + ctrl - h : yabai -m window --resize left:-20:0  || yabai -m window --resize right:-20:0
       cmd + ctrl - l : yabai -m window --resize right:20:0  || yabai -m window --resize left:20:0
       cmd + ctrl - j : yabai -m window --resize bottom:0:20 || yabai -m window --resize top:0:20
       cmd + ctrl - k : yabai -m window --resize top:0:-20   || yabai -m window --resize bottom:0:-20
+      cmd + ctrl - f : yabai -m window --toggle zoom-fullscreen
 
       # float / unfloat window and center on screen
-      cmd - t : yabai -m window --toggle float;\
+      cmd - space : yabai -m window --toggle float;\
                 yabai -m window --grid 4:4:1:1:2:2
 
       # close window yabai way, not overriding system default
