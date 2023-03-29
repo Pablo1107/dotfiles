632469d8c45e4b5a49499357d8bd272c84dfb2a0
Pablo Andres Dealbera <dealberapablo07@gmail.com>
2023-03-29 13:51:55 -0300

    nix: update emacs darwin module

--- modules/darwin/emacs.nix
+++ modules/darwin/emacs.nix
@@ -11,7 +11,24 @@ in
   };
 
   config = mkIf cfg.enable {
-    services.emacs.enable = true;
+    services.emacs = {
+      enable = true;
+      package = pkgs.emacsPgtk;
+      # hardcoded because I don't find any other solution so far
+      additionalPath = [
+        "/opt/homebrew/bin"
+        "/opt/homebrew/sbin"
+        "${config.users.users.pablo.home}/.nix-profile/bin"
+        "/etc/profiles/per-user/pablo/bin"
+        "/run/current-system/sw/bin"
+        "/nix/var/nix/profiles/default/bin"
+        "${config.users.users.pablo.home}/dotfiles/bin"
+        "${config.users.users.pablo.home}/scripts"
+        "${config.users.users.pablo.home}/.local/bin/"
+        "${config.users.users.pablo.home}/.emacs.d/bin"
+        "${config.users.users.pablo.home}/.npm-packages/bin"
+      ];
+    };
 
     # rest of the config is in home manager
   };
