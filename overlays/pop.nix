1a8da2f13e3087162776fe9ca03aee83e1dea5ef
Pablo Andres Dealbera <dealberapablo07@gmail.com>
2023-08-08 15:57:57 -0300

    nix: remove pop overlay

--- overlays/pop.nix
+++ overlays/pop.nix
@@ -1,16 +1,18 @@
 self: pkgs:
 
 {
-  pop = pkgs.pop.override rec {
-    buildGoModule = args: pkgs.buildGoModule ( args // {
-      version = "0.2.0";
-      src = pkgs.fetchFromGitHub {
-        owner = "charmbracelet";
-        repo = "pop";
-        rev = "v0.2.0";
-        sha256 = "1yg3vwrya0bix7y2n0njd6g2g2x18mqxic2mnznhs8s4mnjn8qk4";
-      };
-      vendorHash = "1pz44hdr96g7r7zwrskmhdk9gpdqg84f3ysk7vyx4wblyig0k1zi";
-      ldflags = [ "-s" "-w" "-X=main.Version=0.2.0" ];
-    }); };
+  # pop has been updated to 0.2.0 on nixpkgs-unstable at 53797ef
+  # keeping this in case it gets outdated
+  # pop = pkgs.pop.override rec {
+  #   buildGoModule = args: pkgs.buildGoModule ( args // {
+  #     version = "0.2.0";
+  #     src = pkgs.fetchFromGitHub {
+  #       owner = "charmbracelet";
+  #       repo = "pop";
+  #       rev = "v0.2.0";
+  #       sha256 = "1yg3vwrya0bix7y2n0njd6g2g2x18mqxic2mnznhs8s4mnjn8qk4";
+  #     };
+  #     vendorHash = "1pz44hdr96g7r7zwrskmhdk9gpdqg84f3ysk7vyx4wblyig0k1zi";
+  #     ldflags = [ "-s" "-w" "-X=main.Version=0.2.0" ];
+  #   }); };
 }
