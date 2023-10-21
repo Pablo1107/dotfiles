30ef12599fe466fa30a24985278abfe00e0aaa62
Pablo Andres Dealbera <dealberapablo07@gmail.com>
2023-10-21 02:21:33 -0300

    add fluidsynth

--- config/nixpkgs/home.nix
+++ config/nixpkgs/home.nix
@@ -173,6 +173,13 @@ with pkgs;
     go.enable = true;
   };
 
+  services = {
+    fluidsynth = {
+      enable = true;
+      soundService = "pipewire-pulse";
+    };
+  };
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
