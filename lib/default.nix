65b56a92b5f129b4e5cc8b99a6d63ed94735b3dd
Pablo Andres Dealbera <dealberapablo07@gmail.com>
2023-12-17 02:43:03 -0300

    update lib

--- lib/default.nix
+++ lib/default.nix
@@ -31,6 +31,54 @@ rec {
         })
       { };
 
+  nixVulkanWrapper = pkgs: { bin, package ? pkgs."${bin}" }:
+    pkgs.callPackage
+      (inputs:
+        let pkg = (package.override inputs);
+        in
+        pkgs.stdenv.mkDerivation {
+          name = "${bin}";
+          pname = "${bin}";
+          phases = [ "installPhase" ];
+          installPhase = ''
+            mkdir -p "$out/bin" "$out/share"
+            # cp -pRL "${pkg}/share" "$out/"
+            for f in '${pkg}'/share/*; do # hello emacs */
+            ln -s -t "$out/share/" "$f"
+            done
+            cat >> "$out/bin/${bin}" << EOF
+            #!/usr/bin/env sh
+            ${pkgs.nixgl.nixVulkanIntel}/bin/nixVulkanIntel ${pkg}/bin/${bin} "\$@"
+            EOF
+            chmod u+x  "$out/bin/${bin}"
+          '';
+        })
+      { };
+
+  nixBothWrapper = pkgs: { bin, package ? pkgs."${bin}" }:
+    pkgs.callPackage
+      (inputs:
+        let pkg = (package.override inputs);
+        in
+        pkgs.stdenv.mkDerivation {
+          name = "${bin}";
+          pname = "${bin}";
+          phases = [ "installPhase" ];
+          installPhase = ''
+            mkdir -p "$out/bin" "$out/share"
+            # cp -pRL "${pkg}/share" "$out/"
+            for f in '${pkg}'/share/*; do # hello emacs */
+            ln -s -t "$out/share/" "$f"
+            done
+            cat >> "$out/bin/${bin}" << EOF
+            #!/usr/bin/env sh
+            ${pkgs.nixgl.nixGLIntel}/bin/nixGLIntel ${pkgs.nixgl.nixVulkanIntel}/bin/nixVulkanIntel ${pkg}/bin/${bin} "\$@"
+            EOF
+            chmod u+x  "$out/bin/${bin}"
+          '';
+        })
+      { };
+
   ## Flake Utils
   # System types to support.
   supportedSystems = [ "x86_64-linux" "aarch64-darwin" ];
