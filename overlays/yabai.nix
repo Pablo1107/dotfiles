a211ff779d73e7b723e2405a1b92ebf13533c73f
Pablo Andres Dealbera <dealberapablo07@gmail.com>
2024-02-07 19:58:42 -0300

    update yabai overlay

--- overlays/yabai.nix
+++ overlays/yabai.nix
@@ -1,12 +1,12 @@
 self: pkgs:
 {
   # based on https://github.com/peel/dotfiles/blob/7c4d9a343b02387bdaa429b6b9e903c85a729a6f/overlays/00-basic-pkgs.nix#L7
-  yabai = pkgs.stdenvNoCC.mkDerivation {
+  yabai = pkgs.stdenvNoCC.mkDerivation rec {
     name = "yabai";
-    version = "4.0.1";
+    version = "6.0.9";
     src = pkgs.fetchurl {
-      url = "https://github.com/koekeishiya/yabai/releases/download/v4.0.1/yabai-v4.0.1.tar.gz";
-      sha256 = "sha256-UFtPBftcBytzvrELOjE4vPCKc3CCaA4bpqusok5sUMU=";
+      url = "https://github.com/koekeishiya/yabai/releases/download/v${version}/yabai-v${version}.tar.gz";
+      sha256 = "sha256-QeRtFsHIrfgmfHiX37MxXBE8HATsEw98JgTEJqQceyg=";
     };
 
     dontConfigure = true;
