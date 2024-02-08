c374434aa364b2d86b6d89eec4480318827c8846
Pablo Andres Dealbera <dealberapablo07@gmail.com>
2024-02-08 03:05:07 +0000

    add transmission service to nixos server

--- hosts/server/nixos.nix
+++ hosts/server/nixos.nix
@@ -9,6 +9,16 @@
   personal.reverse-proxy.enable = true;
   personal.clone-dotfiles.enable = true;
 
+  services.transmission = {
+    enable = true; #Enable transmission daemon
+    openRPCPort = true; #Open firewall for RPC
+    settings = {
+      #Override default settings
+      rpc-bind-address = "0.0.0.0"; #Bind to own IP
+      rpc-white-list = "0.0.0.0"; #Whitelist your remote machine (10.0.0.1 in this example)
+    };
+  };
+
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
     (modulesPath + "/profiles/qemu-guest.nix")
