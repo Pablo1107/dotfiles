25f3470182d66e1189c1b6ae30165b16a1d5c4d8
Pablo Andres Dealbera <dealberapablo07@gmail.com>
2024-02-13 03:24:19 +0000

    add wrapWine, amplitube and example pinball overlays

---
+++ overlays/amplitube.nix
@@ -0,0 +1,48 @@
+self: pkgs:
+
+with pkgs;
+
+{
+  amplitube5 =
+    let
+      # installer = wrapWine {
+      #   name = "amplitube5";
+      #   executable = "/var/lib/transmission/Downloads/IK Multimedia - AmpliTube v5.7.0/Setup AmpliTube 5 v5.7.0.exe";
+      #   is64bits = true;
+      #   tricks = [
+      #     "mfc42"
+      #     "vcrun6sp6"
+      #   ];
+      # };
+      bin = wrapWine {
+        name = "amplitube5";
+        executable = "$WINEPREFIX/drive_c/Program\ Files/IK\ Multimedia/AmpliTube\ 5/AmpliTube\ 5.exe";
+        is64bits = true;
+        firstrunScript = ''
+          ${wine}/bin/wine "/var/lib/transmission/Downloads/IK Multimedia - AmpliTube v5.7.0/Setup AmpliTube 5 v5.7.0.exe"
+          ${wine}/bin/wine "/var/lib/transmission/Downloads/IK Multimedia - AmpliTube v5.7.0/R2R/IK_Multimedia_Keygen.exe"
+        '';
+        tricks = [
+          "mfc42"
+          "vcrun6sp6"
+        ];
+      };
+      desktop = makeDesktopItem {
+        name = "Amplitube";
+        desktopName = "Amplitube";
+        icon = fetchurl {
+          url = "https://s.cafebazaar.ir/images/icons/com.ikmultimediaus.android.amplitubeua_512x512.png";
+          sha256 = "sha256-VXS4X9NBE7/Jk9yI5ZS/XhK72tQKyXMZtHRHs1WvUWA=";
+        };
+        type = "Application";
+        exec = "${bin}/bin/amplitube5";
+      };
+    in
+    symlinkJoin {
+      name = "amplitube5";
+      paths = [
+        bin
+        desktop
+      ];
+    };
+}
---
+++ overlays/pinball.nix
@@ -0,0 +1,47 @@
+self: pkgs:
+
+with pkgs;
+
+{
+  # example window application
+  # https://github.com/lucasew/nixcfg/blob/6df64144b74cde4eda64ab53ad42444953024404/nix/pkgs/wineApps/pinball.nix
+  pinball =
+    let
+      wine = wine.override {
+        alsaSupport = true;
+        faudioSupport = true;
+        wineRelease = "staging";
+        wineBuild = "wine32";
+      };
+      rarFile = fetchurl {
+        url = "https://archive.org/download/SpaceCadet_Plus95/Space_Cadet.rar";
+        sha256 = "3cc5dfd914c2ac41b03f006c7ccbb59d6f9e4c32ecfd1906e718c8e47f130f4a";
+      };
+      bin = wrapWine {
+        name = "pinball";
+        executable = "$WINEPREFIX/drive_c/PINBALL.exe";
+        firstrunScript = ''
+          pushd "$WINEPREFIX/drive_c"
+            ${unrar}/bin/unrar x ${rarFile}
+          popd
+        '';
+      };
+      desktop = makeDesktopItem {
+        name = "Pinball";
+        desktopName = "Pinbal - Space Cadet";
+        icon = fetchurl {
+          url = "https://www.chip.de/ii/1/8/8/0/2/9/2/3/028c4582789e6c07.jpg";
+          sha256 = "1lwsnsp4hxwqwprjidgmxksaz13ib98w34r6nxkhcip1z0bk1ilz";
+        };
+        type = "Application";
+        exec = "${bin}/bin/pinball";
+      };
+    in
+    symlinkJoin {
+      name = "pinball-app";
+      paths = [
+        bin
+        desktop
+      ];
+    };
+}
