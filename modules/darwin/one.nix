f0194faf9431abf4ea4fbd729c956dcf42ace2c1
Pablo Andres Dealbera <dealberapablo07@gmail.com>
2024-06-06 23:26:39 -0300

    udpate ONE module

--- modules/darwin/one.nix
+++ modules/darwin/one.nix
@@ -61,5 +61,20 @@ in
         "font-dejavu"
       ];
     };
+
+    # https://github.com/NixOS/nix/issues/8081#issuecomment-1962419263
+    # https://discourse.nixos.org/t/ssl-ca-cert-error-on-macos/31171/6
+    system.activationScripts."ssl-ca-cert-fix".text = ''
+      if [ ! -f /etc/nix/ca_cert.pem ]; then
+        security export -t certs -f pemseq -k /Library/Keychains/System.keychain -o /tmp/certs-system.pem
+        security export -t certs -f pemseq -k /System/Library/Keychains/SystemRootCertificates.keychain -o /tmp/certs-root.pem
+        cat /tmp/certs-root.pem /tmp/certs-system.pem > /tmp/ca_cert.pem
+        sudo mv /tmp/ca_cert.pem /etc/nix/
+      fi
+    '';
+
+    nix.settings = {
+      ssl-cert-file = "/etc/nix/ca_cert.pem";
+    };
   };
 }
