9e65373ea654871d206c24abdb56866c960d361a
Pablo Andres Dealbera <dealberapablo07@gmail.com>
2024-08-07 23:16:50 -0300

    update server

--- hosts/server/nixos.nix
+++ hosts/server/nixos.nix
@@ -28,6 +28,7 @@
   personal.wireguard.enable = true;
   personal.ilv-map.enable = true;
 
+  services.fwupd.enable = true;
 
   services.gvfs.enable = true;
 
@@ -68,6 +69,22 @@
 
   programs.sway = {
     enable = true;
+  };
+  security.polkit.enable = true;
+  systemd = {
+    user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = [ "graphical-session.target" ];
+      wants = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      serviceConfig = {
+          Type = "simple";
+          ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+          Restart = "on-failure";
+          RestartSec = 1;
+          TimeoutStopSec = 10;
+        };
+    };
   };
 
   programs.zsh.enable = true;
