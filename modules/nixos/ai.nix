ecc6d3ffa96295487b9827ffc3851c5f955bdacf
Pablo Andres Dealbera <dealberapablo07@gmail.com>
2024-11-22 13:17:07 -0300

    update ollama using nixpkgs-patched

--- modules/nixos/ai.nix
+++ modules/nixos/ai.nix
@@ -1,4 +1,4 @@
-{ config, options, lib, myLib, pkgs, pkgs-stable, ... }:
+{ config, options, lib, myLib, pkgs, pkgs-patched, pkgs-stable, ... }:
 
 with lib;
 with myLib;
@@ -18,6 +18,7 @@ in
         enable = true;
         port = 11434;
         host = "0.0.0.0";
+        package = pkgs-patched.ollama;
         # acceleration = "cuda";
       };
       open-webui = {
