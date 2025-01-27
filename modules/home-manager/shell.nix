cc7306052a9a017a74e83b3f41033bba42552b68
Pablo Andres Dealbera <dealberapablo07@gmail.com>
2025-01-27 01:59:08 -0300

    add shell-gpt to shell module

--- modules/home-manager/shell.nix
+++ modules/home-manager/shell.nix
@@ -95,6 +95,7 @@ in
       docker-compose
       pop # email client
       pkgs-patched.beancount3
+      shell-gpt
     ];
     home.sessionVariables = mkMerge [ sessionVariables cfg.envVariables ];
     home.sessionPath = cfg.path;
@@ -112,6 +113,20 @@ in
         autoload -U promptinit;
         promptinit
         prompt pure
+
+        # Shell-GPT integration ZSH v0.2
+        _sgpt_zsh() {
+        if [[ -n "$BUFFER" ]]; then
+            _sgpt_prev_cmd=$BUFFER
+            BUFFER+="⌛"
+            zle -I && zle redisplay
+            BUFFER=$(sgpt --shell <<< "$_sgpt_prev_cmd" --no-interaction)
+            zle end-of-line
+        fi
+        }
+        zle -N _sgpt_zsh
+        bindkey ^l _sgpt_zsh
+        # Shell-GPT integration ZSH v0.2
       '';
 
       loginExtra = ''
