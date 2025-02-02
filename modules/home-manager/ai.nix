673764e58f715fe6ecb78aa6c6075be7a995cff1
Pablo Andres Dealbera <dealberapablo07@gmail.com>
2025-02-02 18:28:35 -0300

    feat(ai): enable ai home manager module in server
    * Enable the `ai` module in `home-manager`. This provides access to local AI functionality using the `llm` and `llm-ollama` python packages.
    * Comments are added for the previously used `local-ai` setup, which relied on a separate service.

--- modules/home-manager/ai.nix
+++ modules/home-manager/ai.nix
@@ -12,26 +12,15 @@ in
 
   config = mkIf cfg.enable {
     home.packages = with pkgs; [
-      local-ai
-      local-shell-gpt
+      (python3.withPackages (ps: with ps; [
+        llm
+        llm-ollama
+      ]))
     ];
-    home.sessionVariables = {
-      LOCAL_AI_API_HOST = "http://t14s.local:8080";
-      DEFAULT_MODEL = "stablebeluga-13b.ggmlv3.q6_K.bin";
-    };
-    systemd.user.services.local-ai = {
-      Unit = {
-        Description = "Local AI Service";
-      };
-
-      Service = {
-        ExecStart = "${pkgs.local-ai}/bin/local-ai";
-        WorkingDirectory = "${config.home.homeDirectory}/.local/share/local-ai";
-        Restart = "on-failure";
-      };
-
-      Install = { WantedBy = [ "default.target" ]; };
-    };
+    # home.sessionVariables = {
+    #   LOCAL_AI_API_HOST = "http://t14s.local:8080";
+    #   DEFAULT_MODEL = "stablebeluga-13b.ggmlv3.q6_K.bin";
+    # };
   };
 }
 
