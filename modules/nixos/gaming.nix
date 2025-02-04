6ad5561574a5858bdbda3c45b6f1b30facff3c4c
Pablo Andres Dealbera <dealberapablo07@gmail.com>
2025-02-03 21:12:55 -0300

    feat(modules/nixos/gaming): add QCOW2 image mount for games
    - Adds a `qcow2Path` option to the `gaming` module, allowing users to specify the path to their QCOW2 image.
    - Introduces systemd services to connect and mount the QCOW2 image using qemu-nbd.
    - Adds NTFS filesystem support for mounting the QCOW2 partition.

--- modules/nixos/gaming.nix
+++ modules/nixos/gaming.nix
@@ -10,9 +10,16 @@ in
 {
   options.personal.gaming = {
     enable = mkEnableOption "gaming";
+
+    qcow2Path = mkOption {
+      type = types.str;
+      default = "/var/lib/libvirt/images/games.qcow2";
+      description = "Path to your QCOW2 image";
+    };
   };
 
   config = mkIf cfg.enable {
+    boot.kernelModules = [ "nbd" ];
 
     environment.systemPackages = with pkgs; [
       (lutris.override {
@@ -38,5 +45,32 @@ in
     #     libGLU
     #   ] ++ (pkgs.steam-run.fhsenv.args.multiPkgs pkgs);
     # };
+
+    systemd.services.qemu-nbd-connect = {
+      description = "Connect QCOW2 file using qemu-nbd";
+      before = [ "mnt-qcow2.mount" ];
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = "${pkgs.qemu-utils}/bin/qemu-nbd --connect=/dev/nbd0 ${cfg.qcow2Path}";
+        ExecStop = "${pkgs.qemu-utils}/bin/qemu-nbd --disconnect /dev/nbd0";
+        RemainAfterExit = true;
+      };
+      wantedBy = [ "multi-user.target" ];
+    };
+
+    boot.supportedFilesystems = [ "ntfs" ];
+
+    systemd.mounts = [
+      {
+        description = "Mount QCOW2 NTFS partition";
+        requires = [ "qemu-nbd-connect.service" ];
+        after = [ "qemu-nbd-connect.service" ];
+        what = "/dev/nbd0p2";
+        where = "/shared";
+        type = "ntfs";
+        options = "defaults";
+        wantedBy = [ "multi-user.target" ];
+      }
+    ];
   };
 }
