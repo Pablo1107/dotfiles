7b997eb555421799d30e173e0f4311a9c6e90b8a
Pablo Andres Dealbera <dealberapablo07@gmail.com>
2025-02-05 00:26:03 -0300

    feat(overlays/cfdyndns): update cfdyndns to use cloudflare-rs v0.13
    * Removed the `cargoPatches` attribute as it is no longer needed
    * Updated the source for `cloudflare-rs` to point to the correct branch and commit hash in Pablo1107's fork, using `fetchFromGitHub`.
    * Commented out the `outputHashes` entry for `cloudflare-0.13.0`, as it was not working correctly.

--- overlays/cfdyndns.nix
+++ overlays/cfdyndns.nix
@@ -8,12 +8,15 @@ with pkgs;
       attrs' = builtins.removeAttrs attrs ["cargoHash"];
 
       overrideAttrs = {
-        cargoPatches = [
-          ./cfdyndns/patch.diff
-        ];
+        src = fetchFromGitHub {
+          owner = "Pablo1107";
+          repo = "cfdyndns";
+          rev = "update-cloudflare-rs-to-0.13";
+          sha256 = "sha256-IYhaN3dwkNEs4NOWvMpyKnnKOXElTLUf7yy+Ya0AAvs=";
+        };
         cargoLock = {
           lockFile = ./cfdyndns/Cargo.lock;
-          outputHashes."cloudflare-0.12.0" = "sha256-npT4S1epHABaOeNBA0y2ydeuDtqKG7KX2PVOY25XWpU=";
+          # outputHashes."cloudflare-0.13.0" = "sha256-L7funou+PBMIwmZEkIyZNuAzmFAWgBS3PGgxOnj+aIQ=";
           outputHashes."public-ip-0.2.2" = "sha256-DDdh90EAo3Ppsym4AntczFuiAQo4/QQ9TEPJjMB1XzY=";
         };
       };
