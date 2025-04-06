c0dbf79ad8b450d0f66f4bed7e3f7c7c223b06a0
Pablo Andres Dealbera <dealberapablo07@gmail.com>
2025-04-06 09:09:49 -0300

    update moises-desktop overlay

--- overlays/moises-desktop.nix
+++ overlays/moises-desktop.nix
@@ -10,7 +10,7 @@ let
       "-H"
       "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:123.0) Gecko/20100101 Firefox/123.0"
     ];
-    hash = "sha256-3r6vRzUpjysCPGpJJZe/2fSlGeZV05kw9KcXsSav8uo=";
+    hash = "sha256-Da7CCwDH6Y83ud9MEXImRl17c/ZzLNfvoiE0sGRsEbg=";
   };
   appimageContents = appimageTools.extractType2 { inherit name src; };
 in {
