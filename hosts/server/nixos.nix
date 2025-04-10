9043db4a86227e5a8f141bef301b08bf79c1f3a4
Pablo Andres Dealbera <dealberapablo07@gmail.com>
2025-04-10 19:06:46 -0300

    feat: add hardware.graphics option in nixos

--- hosts/server/nixos.nix
+++ hosts/server/nixos.nix
@@ -42,6 +42,8 @@
   personal.openiscsi.enable = true;
   # personal.jovian.enable = true;
 
+  hardware.graphics.enable = true;
+
   services.flatpak.enable = true;
 
   virtualisation.docker.rootless = {
