4c6fc18e858fe820f67419d28f509d27e1126b46
Pablo Andres Dealbera <dealberapablo07@gmail.com>
2025-07-12 12:53:01 -0300

    feat(config/nvim): Add codecompanion and orgmode plugins
    * Adds the codecompanion plugin for improved coding experience.
    * Adds the orgmode plugin for working with org-mode files.
    * Configures orgmode with basic settings.

--- config/nvim/.config/nvim/lua/personal/plugins/core.lua
+++ config/nvim/.config/nvim/lua/personal/plugins/core.lua
@@ -90,4 +90,31 @@ return {
     {
         'sindrets/diffview.nvim',
     },
+    {
+      "olimorris/codecompanion.nvim",
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-treesitter/nvim-treesitter",
+      },
+      config = true
+    },
+    {
+      'nvim-orgmode/orgmode',
+      event = 'VeryLazy',
+      ft = { 'org' },
+      config = function()
+        -- Setup orgmode
+        require('orgmode').setup({
+          -- org_agenda_files = '~/orgfiles/**/*',
+          -- org_default_notes_file = '~/orgfiles/refile.org',
+        })
+
+        -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
+        -- add ~org~ to ignore_install
+        -- require('nvim-treesitter.configs').setup({
+        --   ensure_installed = 'all',
+        --   ignore_install = { 'org' },
+        -- })
+      end,
+    }
 }
