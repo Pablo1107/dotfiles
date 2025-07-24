3744f4ec7881dcf2a470c53bf4724156311c1dab
Pablo Andres Dealbera <dealberapablo07@gmail.com>
2025-07-24 14:19:29 -0300

    feat(modules/nixos): enable nix garbage collection
    - Added configuration for nix garbage collection to automatically clean up old generations.
    - Configured a randomized delay of 14 minutes to avoid overloading the system.
    - Set the deletion policy to remove generations older than 14 days.
    - This improves disk space usage and system performance by regularly removing unnecessary files.

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -20,6 +20,12 @@ in
       trusted-users = [ "pablo" ];
     };
 
+    nix.gc = {
+      automatic = true;
+      randomizedDelaySec = "14m";
+      options = "--delete-older-than 14d";
+    };
+
     cachix = [
       { name = "nix-community"; sha256 = "0m6kb0a0m3pr6bbzqz54x37h5ri121sraj1idfmsrr6prknc7q3x"; }
       { name = "cuda-maintainers"; sha256 = "08n296wa5f2i4d1v9s94msy7dy5nbz5hs37gy56z86n2i6n9p6jc"; }
