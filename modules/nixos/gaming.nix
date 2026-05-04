4a2463f44bb3b45971ebd9d445b099fd3fa70f24
Pablo Andres Dealbera <dealberapablo07@gmail.com>
2026-05-04 16:13:47 -0300

    update

--- modules/nixos/gaming.nix
+++ modules/nixos/gaming.nix
@@ -294,7 +294,7 @@ in
       # Write information to /etc/xdg/openxr/1/active_runtime.json, VR applications
       # will automatically read this and work with WiVRn (Note: This does not currently
       # apply for games run in Valve's Proton)
-      defaultRuntime = true;
+      # defaultRuntime = true; removed recently
 
       # Run WiVRn as a systemd service on startup
       autoStart = true;
